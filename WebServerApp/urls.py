b954709399041437881bf5751949531c6f3bac82
Gabriel18711 <122276140+Gabriel18711@users.noreply.github.com>
2024-09-17 12:02:33 +0200

    Update urls.py
    Update urls.py7

--- WebServerApp/urls.py
+++ WebServerApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TemperatureReadingList, TemperatureReadingDetail, TemperatureCreateAPIView, LatestTemperatureAPIView, temperature_monitor
+from .views import TemperatureReadingList, TemperatureReadingDetail, TemperatureCreateAPIView, LatestTemperatureAPIView, temperature_monitor, relay_control_view, get_relay_status
 
 urlpatterns = [
     path('temperatures/', TemperatureReadingList.as_view(), name='temperature-list'),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('temperatures/create/', TemperatureCreateAPIView.as_view(), name='temperature-create'),
     path('latest-temperature/', LatestTemperatureAPIView.as_view(), name='latest-temperature'),
     path('', temperature_monitor, name='temperature-monitor'),
+   path('relay-control/', relay_control_view, name='relay_control'),
+    path('getrelaystatus/', get_relay_status, name='get_relay_status'),
 ]
+
